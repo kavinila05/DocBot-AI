90f9761d8e263c94ff22db321d04dac50adbb8a5
kavinila05 <129657981+kavinila05@users.noreply.github.com>
2026-03-31 21:10:48 +0530

    Create ppt_loader.py

---
+++ loaders/ppt_loader.py
@@ -0,0 +1,10 @@
+from pptx import Presentation
+
+def load_ppt(file):
+    prs = Presentation(file)
+    text = ""
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if hasattr(shape, "text"):
+                text += shape.text + "\n"
+    return text
